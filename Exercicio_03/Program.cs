dbacfcf5fc4104cae5827fe68d61b0097422fa1e
Marciocmoliveira <critianomarcio@hotmail.com>
2023-03-05 19:58:37 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_03/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Exercicio_03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Evento evento = new Evento();
+
+        evento.QtdIngressosVendidos = 15;
+        evento.QtdLugares = 30;
+
+        try
+        {
+            if (evento.QtdLugares - evento.QtdIngressosVendidos > 0)
+            {
+                Console.WriteLine("Ingresso validado!");
+                evento.ingressoVendido();
+            }
+            else
+            {
+                throw new IngressoException("Não existem mais lugares" +
+                    " disponíveis para o evento");
+            }
+        }
+        catch (IngressoException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
